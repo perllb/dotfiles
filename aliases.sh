91a99fb3c449d2b68b15a90878fba06e5579d8b5
Base\Per <33324443+perllb@users.noreply.github.com>
2020-04-30 14:49:20 +0200

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -6,7 +6,7 @@ alias ltr='ls -ltrh'
 alias fs1p='cd /fs1/per/'
 alias fs1s='cd /fs1/per/proj/scRNAseq'
 alias wgsrun='singularity exec --bind /fs1 /fs1/resources/containers/wgs_active.sif'
-alias sq='squeue -o "%.18i %.9P %.8j %.8u %.8T %.10M %.9l %.6D %C %c %R" '
+alias sq='squeue -o "%8i %12u %.8M %30j %3t %12P %.5Q %.6m %2C %6E %13R" --sort=-S,p,i'
 alias sqp='sq | grep per'
 alias sqr="sq | grep ' RUNNING'"
 alias sac='sacct'
